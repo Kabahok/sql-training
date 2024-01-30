c915e5be5d2109d8214f843ddb1f359252165eb9
egor avdeev <82036345+Kabahok@users.noreply.github.com>
2024-01-30 12:08:37 +0300

    Update Script-2.sql

--- task2/Script-2.sql
+++ task2/Script-2.sql
@@ -3,7 +3,8 @@ create table task2.orders(
 	date date not null,
 	customer_id int,
 	product_name varchar(20) not null,
-	amount int not null
+	amount int not null,
+	foreign key (customer_id) references task2.customers(customer_id)
 );
 
 
